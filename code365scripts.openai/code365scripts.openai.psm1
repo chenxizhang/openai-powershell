9d8fb073f2bb3acf2807078ca77f2329df8980ea
chenxizhang <ares@xizhang.com>
2023-12-03 21:10:03 +0800

    实现保存promot的功能 Fixes #63

--- code365scripts.openai/code365scripts.openai.psm1
+++ code365scripts.openai/code365scripts.openai.psm1
@@ -3,3 +3,13 @@
 foreach ($directory in @('Public', 'Private')) {
     Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" | ForEach-Object { . $_.FullName }
 }
+
+# check if the "$home\.openai-powershell\profile.ps1" exists, if so, source it, otherwise create it and append some code
+$profilePath = "$home\.openai-powershell\profile.ps1"
+if (Test-Path $profilePath) {
+    . $profilePath
+}
+else {
+    New-Item -Path $profilePath -ItemType File -Force
+    Add-Content -Path $profilePath -Value (Get-Content "$PSScriptRoot\private\profile.ps1" -Raw)
+}
